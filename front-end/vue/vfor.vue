e8a0033ee783637ff0c9c331b9dd44b0010cbb0e
liqin <liqin@pwrd.com>
2019-03-14 11:52:43 +0800

    'update'

---
+++ front-end/vue/vfor.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <div v-for="(item, index) in list" :key="item.id" >{{item.name}}</div>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                list:[
+                    {
+                        id: 1,
+                        name: 'test1',
+                    },
+                    {
+                        id: 2,
+                        name: 'test2',
+                    },
+                    {
+                        id: 3,
+                        name: 'test3',
+                    },
+                ]
+            }
+        },
+        created(){
+            //末尾加一条数据
+            // setTimeout(()=>{
+            //     this.list.push({
+            //         id:4,
+            //         name: 'test4'
+            //     })
+            // },10000)
+            //中间加一条数据
+            setTimeout(()=> {
+                this.list.splice(1,0,{
+                    id:'5',
+                    name: 'test5'
+                })
+            },10000)
+        }
+    }
+</script>
